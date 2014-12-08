a590d380fdc9b7a1dddbbf60508a46081cf36aea
Luong Trong Dai <ltdai91@gmail.com>
2014-12-09 06:35:01 +0700

    demo directives

--- app/modules/posts/js/directives.js
+++ app/modules/posts/js/directives.js
@@ -1,3 +1,26 @@
 'use strict'
 
 angular.module('spBlogger.posts.directives',[]);
+
+angular.module('spBlogger.posts.directives').directive('spbComments', function(Post){
+	return {
+		scope: {
+			postInstance: '='
+		},
+		restrict: 'AEC', // E = Element, A = Attribute, C = Class, M = Comment
+		replace: true,
+		templateUrl: 'modules/posts/views/comments.html',
+		link: function($scope, elem, attrs) {
+			$scope.saveComment = function () {
+				var postId = $scope.postInstance._id,
+					savedPostInstance = {};
+				$scope.comment.datePublished = new Date();	//current datetime
+				angular.copy($scope.postInstance, savedPostInstance);
+				savedPostInstance.comments.unshift($scope.comment);
+				$scope.postInstance.comment.unshift($scope.comment);
+				$scope.comment = {};
+				savedPostInstance.$update();
+			}
+		}
+	};
+});;
