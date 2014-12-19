8c59e5ac8a0c6760f22938d9f00fd9cbb20533e6
luongtrongdai <11123048@st.hcmuaf.edu.vn>
2014-12-19 16:10:47 +0700

    add i18n

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,36 @@
 'use strict'
 
-angular.module('spBlogger',['ngAnimate', 'ui.router', 'ngResource', 'spBlogger.admin', 'spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services']);
+angular.module('spBlogger',['ngAnimate', 'ngSanitize', 'pascalprecht.translate', 'ui.router', 'ngResource', 'spBlogger.admin', 'spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services']);
 
 angular.module('spBlogger').value('version','V1.0');
 
 //Go to allPosts when start
-angular.module('spBlogger').run(['$state', function($state){
+angular.module('spBlogger').run(['$state', '$rootScope', '$translate', function($state, $rootScope, $translate){
 		$state.go('allPosts');
+
+		$rootScope.languagePreference = {currentLanguage: 'en'};
+
+		$rootScope.languagePreference.switchLanguage = function(key) {
+			$translate.use(key);
+			$rootScope.languagePreference.currentLanguage = key;
+		}
 	}]);
+
+angular.module('spBlogger')
+	.config(['$translateProvider', function($translateProvider) {
+		$translateProvider.translations('en', {
+												TITLE: 'The Single Page Blogger',
+												SUBTITLE: 'One Stop Blogging Solution',
+												COMMENTS: 'Comments',
+												BY:'By',
+												ADD:'Add'
+												});
+		$translateProvider.translations('de', {
+												TITLE: 'Single Page Blogger',
+												SUBTITLE: 'Die Komplettlösung für Ihr Blog',
+												COMMENTS: 'Kommentare',
+												BY:'Von',
+												ADD:'Hinzufügen'
+												});
+	$translateProvider.preferredLanguage('en');
+}]);
